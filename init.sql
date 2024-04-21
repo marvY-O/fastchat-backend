39dc5b168adca810d0a64dae5baa05f6786ad0dd
Vyom Verma <vyom.verma@internal.utradesolutions.com>
2024-04-21 22:31:18 +0530

    Updated initialization SQL script to include new tables

--- init.sql
+++ init.sql
@@ -8,3 +8,25 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS chat (
+    chat_id SERIAL PRIMARY KEY,
+    user1_id UUID NOT NULL,
+    user2_id UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_message_time TIMESTAMP,
+    FOREIGN KEY (user1_id) REFERENCES users(id),
+    FOREIGN KEY (user2_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS message (
+    message_id SERIAL PRIMARY KEY,
+    chat_id INT NOT NULL,
+    sender_id UUID NOT NULL,
+    receiver_id UUID NOT NULL,
+    content TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chat(chat_id),
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_id) REFERENCES users(id)
+);
